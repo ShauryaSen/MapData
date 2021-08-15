21246c26e5d7083c032a59c37ef0ab7d05cf4a69
ShauryaSen <shauryasen12@gmail.com>
2021-08-15 15:17:25 -0400

    what

---
+++ src/Map.java
@@ -0,0 +1,33 @@
+import processing.core.*;
+
+public class Map extends PApplet {
+	
+	public static void main(String[] args) {
+		PApplet.main("Map");
+	}
+
+	public void setup() {
+		background(0,134, 214);
+		size(800, 500);
+	}
+	
+	public void draw() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
