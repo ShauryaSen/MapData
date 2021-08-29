5aad76b29371bafa841c8d16f2d68b8b559ea309
ShauryaSen <shauryasen12@gmail.com>
2021-08-29 12:42:49 -0400

    csv parsing

--- src/data/ParseData.java
+++ src/data/ParseData.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.util.HashMap;
 import java.util.List;
 
 import de.fhpotsdam.unfolding.data.Feature;
@@ -9,15 +10,39 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
 public class ParseData {
-	
-	public static List<Marker> populationDensityParser(PApplet window) {
+
 	// return awesome colored markers so in the main script i can just be like map.addMarkers(...
+	public static List<Marker> populationDensityParser(PApplet window) {
 		List<Feature> countries = GeoJSONReader.loadData(window, "data/countries.geo.json");
-		List<Marker> countryMarkers = MapUtils.createSimpleMarkers(countries);
+		List<Marker> countryMarkers = MapUtils.createSimpleMarkers(countries);	
 		
+		/*
+		 *  put CSV data into dictionary with it's id as key and the population 
+		 *  density has value
+		 */
+		
+		// load the rows in the csv file into an array
+		String[] rows = window.loadStrings("data/countries-population-density.csv");
+		HashMap<String, String> countryData = new HashMap<String, String>();
+		
+		for (String row : rows) {
+			// the data in each row is split by these: ";" 
+			String[] columns = row.split(";");
+			
+			// apparently some of it doesn't even have three columns
+			if (columns.length == 3) {
+				// columns[1] is the country id and columns[2] is the pop. density
+				countryData.put(columns[1], columns[2]);
+			}
+
+			
+			
+		}
 		// start off on 255, 255, 255 markers (white) and then decrease the blue and green to make it more red when popuation increases
 		for (Marker marker : countryMarkers) {
-			marker.setColor(window.color(255, 100, 100));
+			marker.setColor(window.color(255, 200, 200, 200));
+			
+			
 		}
 		
 		return countryMarkers;
